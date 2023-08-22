99ab16379ebf24bb868ba3e95ee2b5de8d7501a5
Reis567 <matheusdosreis9@gmail.com>
2023-08-22 08:26:15 -0300

    js consts

---
+++ app.js
@@ -0,0 +1,7 @@
+const passwordBox = document.getElementById('password');
+const lenght = 10;
+
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+const number = "0123456789";
+const symbol = ";!@#$%&*-(){}";
